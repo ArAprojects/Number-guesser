4b7647d33f2d7275a7ab836bfd74489e3f375dbf
Brian B <jyh887@gmail.com>
2019-03-22 12:36:50 -0600

    Refactored JS and added more classes to index.html

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-var button = document.querySelector(".submit-button");
-var challengerNameUno = document.querySelectorAll(".small-title-challenger-one");
+var updateButton = document.querySelector(".update-button");
+var minInputDisplay = document.querySelector(".min-range");
+var maxInputDisplay = document.querySelector(".max-range");
+var minInput = document.querySelector(".min-range-input");
+var maxInput = document.querySelector(".max-range-input");
+var challengerNameUno = document.querySelector(".small-title-challenger-one-instance-one")
 var challengerName1 = document.querySelector(".challenger-one-name")
-var challengerNameDos = document.querySelectorAll(".small-title-challenger-two");
+var challengerNameDos = document.querySelector(".small-title-challenger-two-instance-one");
 var challengerName2 = document.querySelector(".challenger-two-name")
+var button = document.querySelector(".submit-button");
+var player1Guess = document.querySelector(".challenger-one-guess");
+var player2Guess = document.querySelector(".challenger-two-guess");
+var player1CurrentGuess = document.querySelector(".current-guess-one");
+var player2CurrentGuess = document.querySelector(".current-guess-two");
 
 
-/* This is for entering the names of the players*/
+updateButton.addEventListener("click", updateRange);
 
-button.addEventListener("click", function () {
-	for(i = 0; i < challengerNameUno.length;i++) {
+function updateRange(e) {
+	e.preventDefault();
+	minInputDisplay.textContent = minInput.value;
+	maxInputDisplay.textContent = maxInput.value;
+};
+
+button.addEventListener("click", updateChallengerNamesAndGuess);
+
+function updateChallengerNamesAndGuess(e) {
+	e.preventDefault(e);
+	challengerName2.textContent = challengerName2.value;
+	challengerNameDos.textContent = challengerName2.textContent;
 	challengerName1.textContent = challengerName1.value
-	challengerNameUno[i].textContent = challengerName1.textContent;
+	challengerNameUno.textContent = challengerName1.textContent;
+	player1CurrentGuess.textContent = player1Guess.value;
+	player2CurrentGuess.textContent = player2Guess.value;
 }
-	})
-button.addEventListener("click", function () {
-	for(i = 0; i < challengerNameDos.length;i++) {
-	challengerName2.textContent = challengerName2.value
-	challengerNameDos[i].textContent = challengerName2.textContent;
+
+button.addEventListener("click", createRandomNumber);
+
+function createRandomNumber(e) {
+	e.preventDefault(e);
+	newNumberGenerated = Math.floor(Math.random() * (+maxInput.value - +minInput.value)) + +minInput.value;
 }
-	})
+
+
+
+
+
+
 
 
 
