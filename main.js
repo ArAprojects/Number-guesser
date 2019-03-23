815428f080e4d437797bf8e917ca7fb4ed96c330
Brian B <jyh887@gmail.com>
2019-03-23 12:10:25 -0600

    Added changes to update and submit button. Added more classes to HTML

--- main.js
+++ main.js
@@ -12,17 +12,40 @@ var player1Guess = document.querySelector(".challenger-one-guess");
 var player2Guess = document.querySelector(".challenger-two-guess");
 var player1CurrentGuess = document.querySelector(".current-guess-one");
 var player2CurrentGuess = document.querySelector(".current-guess-two");
+var tooHighOrLow1 = document.querySelector(".high-or-low-1");
+var tooHighOrLow2 = document.querySelector(".high-or-low-2");
+var resetGame = document.querySelector(".container-challenger");
+var resetGame4Real = document.querySelector(".reset-button")
 
 
-updateButton.addEventListener("click", updateRange);
+
+updateButton.addEventListener("click", updateRange); //Come back to
+
+/*function updateRange() {
+	if (maxInput.value === ""  || minInput.value === "") {
+		updateButton.disabled = true;
+	} else {
+		updateButton.disabled = false;
+		minInputDisplay.textContent = minInput.value;
+		maxInputDisplay.textContent = maxInput.value;
+	}
+
+	
+};*/
 
 function updateRange(e) {
-	e.preventDefault();
-	minInputDisplay.textContent = minInput.value;
-	maxInputDisplay.textContent = maxInput.value;
-};
+	e.preventDefault(e);
+	if (maxInput.value. > -1 && minInput.value. > -1) {
+		updateButton.disabled = false;
+		console.log(minInput.value)
 
-button.addEventListener("click", updateChallengerNamesAndGuess);
+	}
+	minInputDisplay.textContent = minInput.value;
+		maxInputDisplay.textContent = maxInput.value;
+}
+
+
+button.addEventListener("click", updateChallengerNamesAndGuess); // This gets user names and guesses
 
 function updateChallengerNamesAndGuess(e) {
 	e.preventDefault(e);
@@ -34,12 +57,54 @@ function updateChallengerNamesAndGuess(e) {
 	player2CurrentGuess.textContent = player2Guess.value;
 }
 
-button.addEventListener("click", createRandomNumber);
+button.addEventListener("click", createRandomNumber); // This creates a random number after the submit button is clicked
 
 function createRandomNumber(e) {
 	e.preventDefault(e);
 	newNumberGenerated = Math.floor(Math.random() * (+maxInput.value - +minInput.value)) + +minInput.value;
 }
+
+
+button.addEventListener("click", tooHighOrLow) // This will tell player 1 if there guess is too high or low
+
+function tooHighOrLow(e) {
+	e.preventDefault(e);
+	if (player1Guess.value < newNumberGenerated) {
+		tooHighOrLow1.textContent = "That's too low"
+	} else if (player1Guess.value > newNumberGenerated) {
+		tooHighOrLow1.textContent = "That's too high"
+	} else {
+		tooHighOrLow1.textContent = "You got it!"
+	}
+}
+
+button.addEventListener("click", tooHighOrLow3) // This will tell player 2 if their guess is too high or low
+
+function tooHighOrLow3(e) {
+	e.preventDefault(e);
+	if (player2Guess.value < newNumberGenerated) {
+		tooHighOrLow2.textContent = "That's too low"
+	} else if (player2Guess.value > newNumberGenerated) {
+		tooHighOrLow2.textContent = "That's too high"
+	} else {
+		tooHighOrLow2.textContent = "You got it!"
+	}
+}
+
+resetGame4Real.addEventListener("click", resetCurrentGame);
+
+function resetCurrentGame(e) {
+	document.getElementsByClassName("container-challenger").innerHTML = "";
+	}
+
+
+
+
+
+
+
+
+
 
 
 
